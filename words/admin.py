fbb5d2c825c6eebbb4ffcbcacccde756c12f0cf5
Jade Moon <jadeym1222@naver.com>
2024-08-24 22:37:33 +0900

    words admin 페이지 구축
    생성한 모델을 admin 페이지에서도 확인하기 위해  admin 파일을 구축했다.
    
    이때, sentece의 list_display에는 다대다 관계로 연결된  word가 들어가지 않아 메서드를 작성한 후, 메서드명을 list_display에 넣어주었다.

--- words/admin.py
+++ words/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from words.models import Word, Sentence, Vocabulary, Quiz, Memo
 
 # Register your models here.
+@admin.register(Word)
+class WordAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "word",
+        "definition",
+        "topic",
+        "difficulty",
+    ]
+
+@admin.register(Sentence)
+class SentenceAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "sentence",
+        "definition",
+        "source",
+        "get_words",
+    ]
+
+    def get_words(self):
+        return ", ".join([word.word for word in self.word.all()])
+    get_words.short_description = "Words"  # 관리자 페이지에 표시할 이름
+
+@admin.register(Vocabulary)
+class VocabularyAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "word",
+        "added_at",
+    ]
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "word",
+        "sentence",
+        "user_answer",
+        "is_correct",
+        "quiz_date",
+    ]
+
+@admin.register(Memo)
+class MemoAdmin(admin.ModelAdmin):
+    list_display = [
+        "id",
+        "user",
+        "content",
+        "created_at",
+        "updated_at",
+    ]
