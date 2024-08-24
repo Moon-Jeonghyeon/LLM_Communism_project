77dddb76071920b37b42174f461ad3be5c4d7ab8
Jade Moon <jadeym1222@naver.com>
2024-08-24 15:41:01 +0900

    User 모델 생성
    커스텀 유저 모델을 생성함.
    
    - edu_prj/settings.py
    AUTH_USER_MODEL 설정 추가,
    템플릿 DIR 미리 추가,
    언어 설정은 한국어로, 시간은 서울 시간으로 설정했다.
    이미지 필드인 프로필 사진을 위해 미디어루트도 설정했다.
    
    - users/models.py
    AbstractUser를 상속받아 커스텀 유저 모델을 생성했다.
    email, profile_image, joined_at 컬럼을 추가했다.
    
    - users/admin.py
    admin 페이지에 보여질 목록을 작성함.
    이때, admin 페이지에서 사용자 객체를 클릭했을 때 non-editable Exception 오류가 발생했는데 이는 auto_now_add 설정값을 가진 joined_at 필드가 수정이 가능한 필드 영역으로 입력되었기 때문이었다.
    이를 해결하기 위해 readonly_fields 에 joined_at 을 추가하여 수정이 불가능한 필드로 만들어주었고, admin 페이지에서 사용자 객체에 접근할 수 있게 되었다.

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from users.models import User
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = [
+        (None, {"fields": ("username", "password")}),
+        ("개인정보", {"fields": ("first_name", "last_name", "email")}),
+        ("추가필드", {"fields": ("profile_image",)}),
+        (
+            "권한",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                )
+            }
+        ),
+        ("중요한 일정", {"fields": ("last_login", "joined_at")}),
+    ]
+    readonly_fields = ["joined_at"]
--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class User(AbstractUser):
+    email = models.EmailField("이메일", blank=True, null=True)
+    profile_image = models.ImageField("프로필 이미지", upload_to='user/profile', blank=True, null=True)
+    joined_at = models.DateTimeField("가입일", auto_now_add=True)
+
+    def __str__(self):
+        return self.username
